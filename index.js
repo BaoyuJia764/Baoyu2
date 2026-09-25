997945dbe63a076157b4de2ac7a34d486f1636dc
BaoyuJia764 <baoyujia764@gmail.com>
2026-09-25 12:55:29 -0500

    Launcher
    yeah so html dont work LOL

---
+++ index.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: { nodeIntegration: false }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
